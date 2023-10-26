900c22e8f572d77b7da172b0c4a83d1a1d0958d4
rikimario <mario_martinov@abv.bg>
2023-10-26 10:25:12 +0300

    add switch message

--- components-basics/src/components/Conter.jsx
+++ components-basics/src/components/Conter.jsx
@@ -1,34 +1,58 @@
 import { useState } from "react";
 
 export default function Counter(props) {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  const onIncrement = () => {
-    setCount(count + 1);
-  }
+    const onIncrement = () => {
+        setCount(count + 1);
+    }
 
-  const clearCounter = () => {
-    setCount(0)
-  }
+    const clearCounter = () => {
+        setCount(0)
+    }
+
+    let message = null;
+
+    switch (count) {
+        case 1:
+            message = 'First blood'
+            break;
+        case 2:
+            message = 'Double kill'
+            break;
+        case 3:
+            message = 'Triple Kill'
+            break;
+        case 3:
+            message = 'Multi Kill'
+            break;
+        case 4:
+            message = 'Unstoppable'
+            break;
+        case 5:
+            message = 'God like'
+            break;
+        default:
+            message = 'Monster kill';
+    }
 
 
+    return (
+        <div>
+            <h3>Counter</h3>
 
-  return (
-    <div>
-      <h3>Counter</h3>
+            {count < 0
+                ? <p>Invalid count!</p>
+                : null
+            }
 
-      {count < 0
-        ? <p>Invalid count!</p>
-        : null
-      }
+            {count == 0 && <p>Please start incrementing!</p>}
+            <h4>{message}</h4>
+            <p>Count: {count}</p>
 
-      {count == 0 && <p>Please start incrementing!</p>}
-
-      <p>Count: {count}</p>
-
-      <button onClick={onIncrement}>+</button>
-      <button onClick={clearCounter}>Clear</button>
-      <button onClick={() => { setCount(count - 1) }}>-</button>
-    </div>
-  )
+            <button onClick={onIncrement}>+</button>
+            <button onClick={clearCounter}>Clear</button>
+            <button onClick={() => { setCount(count - 1) }}>-</button>
+        </div>
+    )
 }
