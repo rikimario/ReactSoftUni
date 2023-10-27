032478bd227cb81a02fb475eb7486e8426a91c28
rikimario <mario_martinov@abv.bg>
2023-10-27 17:06:39 +0300

    adding movie list

---
+++ components-deep-dive/src/components/Movie.jsx
@@ -0,0 +1,21 @@
+export default function Movie({
+    title,
+    year,
+    plot,
+    posterUrl,
+    director,
+}) {
+
+    return (
+        <article>
+            <h3>{title}, {year}</h3>
+            <main>
+                <img src={posterUrl} alt={title} />
+                <p>{plot}</p>
+            </main>
+            <footer>
+                <p>director: {director}</p>
+            </footer>
+        </article>
+    )
+};
