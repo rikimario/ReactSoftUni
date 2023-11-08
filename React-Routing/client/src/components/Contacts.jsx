f523f826315a015472c093baf6f597ecefad2ad5
rikimario <mario_martinov@abv.bg>
2023-11-08 19:37:14 +0200

    add contacts

---
+++ React-Routing/client/src/components/Contacts.jsx
@@ -0,0 +1,17 @@
+const Contacts = () => {
+    return (
+        <>
+            <h2>Contacts Page</h2>
+
+            <label htmlFor="">Title</label>
+            <input type="text" /><br />
+            <label htmlFor="">Description</label>
+            <textarea name="" id="" cols="30" rows="10"></textarea><br />
+            <label htmlFor="">Submit</label>
+            <input type="submit" value="" />
+
+        </>
+    )
+};
+
+export default Contacts;
