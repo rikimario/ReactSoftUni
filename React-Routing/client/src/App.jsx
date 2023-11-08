0990b251bf9bd928928cc09828aa11ccf9219659
rikimario <mario_martinov@abv.bg>
2023-11-08 19:36:52 +0200

    add routs

--- React-Routing/client/src/App.jsx
+++ React-Routing/client/src/App.jsx
@@ -1,5 +1,10 @@
+
+import { Routes, Route } from 'react-router-dom';
 import './App.css'
 import Navigation from './components/Navigation.jsx';
+import Home from './components/Home';
+import About from './components/About.jsx';
+import Contacts from './components/Contacts.jsx';
 
 function App() {
 
@@ -8,6 +13,12 @@ function App() {
       <h1>Rect Router</h1>
 
       <Navigation />
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contacts' element={<Contacts />} />
+      </Routes>
 
       <footer>All rights reserved &copy;</footer>
     </>
