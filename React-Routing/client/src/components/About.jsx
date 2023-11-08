117d085f29bd64cd2ad87967089e5e0e77ae2239
rikimario <mario_martinov@abv.bg>
2023-11-08 19:37:06 +0200

    add about route

---
+++ React-Routing/client/src/components/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return (
+        <>
+            <h2>About</h2>
+
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores odio nihil tempora labore, culpa autem aut doloribus ipsa laborum ut mollitia numquam expedita voluptatum harum at eaque nisi possimus ducimus!</p>
+        </>
+    );
+};
+
+export default About;
