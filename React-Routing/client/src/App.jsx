d92e0f75bc16d11f9215032477765aac93f92381
rikimario <mario_martinov@abv.bg>
2023-11-08 19:24:46 +0200

    add h1 footer and navigation

--- React-Routing/client/src/App.jsx
+++ React-Routing/client/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navigation from './components/Navigation.jsx';
 
 function App() {
 
   return (
     <>
-      <button>Login</button>
+      <h1>Rect Router</h1>
+
+      <Navigation />
+
+      <footer>All rights reserved &copy;</footer>
     </>
   )
 }
