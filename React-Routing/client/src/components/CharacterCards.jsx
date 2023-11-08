ba212d038edfdcd0c0269a6d51211050db76fc7f
rikimario <mario_martinov@abv.bg>
2023-11-09 00:47:46 +0200

    add character cards

---
+++ React-Routing/client/src/components/CharacterCards.jsx
@@ -0,0 +1,30 @@
+import Button from 'react-bootstrap/Button';
+import Card from 'react-bootstrap/Card';
+import { Link } from 'react-router-dom';
+
+const CharacterCards = ({
+    name,
+    hair_color,
+    eye_color,
+    birth_year,
+    gander,
+}) => {
+    return (
+        <Card style={{ width: '18rem' }}>
+            <Card.Body>
+                <Card.Title>{name}</Card.Title>
+                <Card.Text>
+                    <ul>
+                        <li>hair_color: {hair_color}</li>
+                        <li>eye_color: {eye_color}</li>
+                        <li>birth_year: {birth_year}</li>
+                        <li>gander: {gander}</li>
+                    </ul>
+                </Card.Text>
+                <Button as={Link} to={`/characters/${normalizeName}`} variant="primary">Details</Button>
+            </Card.Body>
+        </Card>
+    );
+}
+
+export default CharacterCards;
