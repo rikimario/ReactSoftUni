931e1ec3271f481105fbf9e7e2789327f485926b
rikimario <mario_martinov@abv.bg>
2023-11-08 19:24:29 +0200

    navigation

---
+++ React-Routing/client/src/components/Navigation.jsx
@@ -0,0 +1,10 @@
+const Navigation = () => {
+    return (
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+        </nav>
+    );
+};
+
+export default Navigation;
