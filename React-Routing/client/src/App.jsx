b3d074af1b64ee61d34a09266a13f475f359a6a7
rikimario <mario_martinov@abv.bg>
2023-11-08 21:11:00 +0200

    add characterlist to app.jsx

--- React-Routing/client/src/App.jsx
+++ React-Routing/client/src/App.jsx
@@ -1,23 +1,22 @@
 
 import { Routes, Route } from 'react-router-dom';
-import './App.css'
 import Navigation from './components/Navigation.jsx';
 import Home from './components/Home';
 import About from './components/About.jsx';
 import Contacts from './components/Contacts.jsx';
+import CharacterList from './components/Characters.jsx';
 
 function App() {
 
   return (
     <>
-      <h1>Rect Router</h1>
-
       <Navigation />
 
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/contacts' element={<Contacts />} />
+        <Route path='/characters' element={<CharacterList />} />
       </Routes>
 
       <footer>All rights reserved &copy;</footer>
