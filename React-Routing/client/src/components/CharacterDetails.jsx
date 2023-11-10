7513ff8a52d1f3c75397a7197f92e87655296e3c
rikimario <mario_martinov@abv.bg>
2023-11-10 23:14:04 +0200

    add char details

---
+++ React-Routing/client/src/components/CharacterDetails.jsx
@@ -0,0 +1,12 @@
+const CharacterDetails = () => {
+    const name = 'Mario'
+    return (
+        <>
+            <h1>{name}</h1>
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore modi delectus earum aperiam laboriosam fugiat magnam, quam culpa eos hic corporis nemo cupiditate aliquam enim nisi, odio dolorum, eveniet voluptate.</p>
+        </>
+    );
+};
+
+export default CharacterDetails;
