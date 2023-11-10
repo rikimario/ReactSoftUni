fab709ea27b7370703a4b16220a4a36a5eb3467e
rikimario <mario_martinov@abv.bg>
2023-11-10 23:24:06 +0200

    add useEffect, useState and useParams to char details

--- React-Routing/client/src/components/CharacterDetails.jsx
+++ React-Routing/client/src/components/CharacterDetails.jsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 const CharacterDetails = () => {
-    const name = 'Mario'
+    const { id } = useParams();
+    const [character, setCharacter] = useState({});
+
+    useEffect(() => {
+        fetch(`https://swapi.dev/api/people/${id}`)
+            .then(res => res.json())
+            .then(setCharacter);
+    }, [id]);
+
     return (
         <>
-            <h1>{name}</h1>
+            <h1>{character.name}</h1>
 
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Inventore modi delectus earum aperiam laboriosam fugiat magnam, quam culpa eos hic corporis nemo cupiditate aliquam enim nisi, odio dolorum, eveniet voluptate.</p>
         </>
