d012ad257c6c2ced756107bf24603a328f131135
rikimario <mario_martinov@abv.bg>
2023-11-11 12:38:36 +0200

    add about jsx

---
+++ React-Routing/client/src/components/AboutUs.jsx
@@ -0,0 +1,11 @@
+
+const AboutUs = () => {
+    return (
+        <>
+            <h4>About Us</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores odio nihil tempora labore, culpa autem aut doloribus ipsa laborum ut mollitia numquam expedita voluptatum harum at eaque nisi possimus ducimus!</p>
+        </>
+    );
+};
+
+export default AboutUs;
