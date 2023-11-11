e561c4c98892e93ff2e9dfec6fbfec6117d3a56d
rikimario <mario_martinov@abv.bg>
2023-11-11 12:38:29 +0200

    add mission jsx

---
+++ React-Routing/client/src/components/Mission.jsx
@@ -0,0 +1,11 @@
+
+const Mission = () => {
+    return (
+        <>
+            <h4>Mission</h4>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Asperiores odio nihil tempora labore, culpa autem aut doloribus ipsa laborum ut mollitia numquam expedita voluptatum harum at eaque nisi possimus ducimus!</p>
+        </>
+    );
+};
+
+export default Mission;
