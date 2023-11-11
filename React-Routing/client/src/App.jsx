023d56a9bf5599f3f57147e3742668f42ca58c7e
rikimario <mario_martinov@abv.bg>
2023-11-11 12:40:01 +0200

    nest routs to about route and import about us, mission and values pages

--- React-Routing/client/src/App.jsx
+++ React-Routing/client/src/App.jsx
@@ -7,6 +7,9 @@ import Contacts from './components/Contacts.jsx';
 import CharacterList from './components/CharacterList.jsx';
 import CharacterDetails from './components/CharacterDetails.jsx';
 import NotFound from './components/NotFound.jsx';
+import AboutUs from './components/AboutUs.jsx';
+import Mission from './components/Mission.jsx';
+import Values from './components/Values.jsx';
 
 
 function App() {
@@ -17,7 +20,11 @@ function App() {
 
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='/about' element={<About />} />
+        <Route path='/about' element={<About />}>
+          <Route path="us" element={<AboutUs />} />
+          <Route path="mission" element={<Mission />} />
+          <Route path="values" element={<Values />} />
+        </Route>
         <Route path='/contacts' element={<Contacts />} />
         <Route path='/characters' element={<CharacterList />} />
         <Route path='/characters/:id' element={<CharacterDetails />} />
