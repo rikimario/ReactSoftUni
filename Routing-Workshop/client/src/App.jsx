f56a7638c74d4b50759adf4640281fc0b3ddb0a5
rikimario <mario_martinov@abv.bg>
2023-11-19 18:29:43 +0200

    add header to app

--- Routing-Workshop/client/src/App.jsx
+++ Routing-Workshop/client/src/App.jsx
@@ -1,8 +1,10 @@
+import Header from "./components/Header/Header";
+
 function App() {
   return (
-    <>
-      <h1>Hello from The Bed!</h1>
-    </>
+    <div id="box">
+      <Header />
+    </div>
   );
 }
 
