22c2475138e7ae2debd30105d72529f4864d64f9
rikimario <mario_martinov@abv.bg>
2023-11-20 01:41:59 +0200

    add routes for create-game, login and register

--- Routing-Workshop/client/src/App.jsx
+++ Routing-Workshop/client/src/App.jsx
@@ -2,6 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import GameList from "./components/GameList/GameList";
+import Create from "./components/Create/Create";
+import Login from "./components/Login/Login";
+import Register from "./components/Register/Register";
 
 function App() {
   return (
@@ -10,7 +13,10 @@ function App() {
 
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/game-list" element={<GameList />} />
+        <Route path="/games" element={<GameList />} />
+        <Route path="/create-game" element={<Create />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </div>
   );
