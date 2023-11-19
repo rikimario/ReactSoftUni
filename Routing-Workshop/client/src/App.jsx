4eb6fa237fe9351d4eef2e246ccfc8f92ce413f4
rikimario <mario_martinov@abv.bg>
2023-11-19 18:59:52 +0200

    add routes to app

--- Routing-Workshop/client/src/App.jsx
+++ Routing-Workshop/client/src/App.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import GameList from "./components/GameList/GameList";
 
 function App() {
   return (
     <div id="box">
       <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/game-list" element={<GameList />} />
+      </Routes>
     </div>
   );
 }
