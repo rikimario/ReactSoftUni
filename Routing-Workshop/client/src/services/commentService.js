9d72a1e478a48344ef643802ad07c80f528d1b32
rikimario <mario_martinov@abv.bg>
2023-11-21 01:39:17 +0200

    add get all to comment service

--- Routing-Workshop/client/src/services/commentService.js
+++ Routing-Workshop/client/src/services/commentService.js
@@ -1,9 +1,19 @@
-import * as requset from "../lib/request";
+import * as request from "../lib/request";
 
 const baseURL = "http://localhost:3030/jsonstore/comments";
 
+export const getAll = async (gameId) => {
+  const query = new URLSearchParams({
+    where: `gameId=${gameId}`,
+  });
+
+  const result = await request.get(`${baseURL}`);
+
+  return Object.values(result).filter((comment) => comment.gameId === gameId);
+};
+
 export const create = async (gameId, username, text) => {
-  const newComment = await requset.post(baseURL, {
+  const newComment = await request.post(baseURL, {
     gameId,
     username,
     text,
