9573f350a8691ba637ea8cde0fa0b15c01c4ea1a
rikimario <mario_martinov@abv.bg>
2023-11-20 15:02:48 +0200

    add create handler

--- Routing-Workshop/client/src/components/Create/Create.jsx
+++ Routing-Workshop/client/src/components/Create/Create.jsx
@@ -1,7 +1,13 @@
 export default function Create() {
+  const createGameSubmitHandler = (e) => {
+    e.preventDefault();
+
+    const gameData = Object.fromEntries(new FormData(e.currentTarget));
+  };
+
   return (
     <section id="create-page" className="auth">
-      <form id="create">
+      <form id="create" onSubmit={createGameSubmitHandler}>
         <div className="container">
           <h1>Create Game</h1>
           <label htmlFor="leg-title">Legendary title:</label>
