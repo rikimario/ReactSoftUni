d3dfd91dcc10714e7e821b836e2208c3af2d9959
rikimario <mario_martinov@abv.bg>
2023-11-20 16:48:15 +0200

    add useEffect and useState to game list

--- Routing-Workshop/client/src/components/GameList/GameList.jsx
+++ Routing-Workshop/client/src/components/GameList/GameList.jsx
@@ -1,6 +1,13 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import * as gameService from "../../services/gameService";
 
-export default function GameList() {
+export default function GameList({ title, category, maxLevel, name, summary }) {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    gameService.getAll().then((result) => setGames(result));
+  }, []);
   return (
     <section id="catalog-page">
       <h1>All Games</h1>
@@ -15,26 +22,7 @@ export default function GameList() {
           </Link>
         </div>
       </div>
-      <div className="allGames">
-        <div className="allGames-info">
-          <img src="./images/avatar-1.jpg" />
-          <h6>Action</h6>
-          <h2>Zombie lang</h2>
-          <a href="#" className="details-button">
-            Details
-          </a>
-        </div>
-      </div>
-      <div className="allGames">
-        <div className="allGames-info">
-          <img src="./images/avatar-1.jpg" />
-          <h6>Action</h6>
-          <h2>MineCraft</h2>
-          <a href="#" className="details-button">
-            Details
-          </a>
-        </div>
-      </div>
+
       {/* Display paragraph: If there is no games  */}
       <h3 className="no-articles">No articles yet</h3>
     </section>
