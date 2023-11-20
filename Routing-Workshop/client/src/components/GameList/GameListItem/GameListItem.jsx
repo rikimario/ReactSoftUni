a66e2176389f1194d845b87f6579fba315289c33
rikimario <mario_martinov@abv.bg>
2023-11-20 23:04:17 +0200

    fixed path in gamel list item from details to games

--- Routing-Workshop/client/src/components/GameList/GameListItem/GameListItem.jsx
+++ Routing-Workshop/client/src/components/GameList/GameListItem/GameListItem.jsx
@@ -1,13 +1,13 @@
 import { Link } from "react-router-dom";
 
-export default function GameListItem({ title, category, imageUrl }) {
+export default function GameListItem({ _id, title, category, imageUrl }) {
   return (
     <div className="allGames">
       <div className="allGames-info">
         <img src={imageUrl} />
         <h6>{category}</h6>
         <h2>{title}</h2>
-        <Link to="/details" className="details-button">
+        <Link to={`/games/${_id}`} className="details-button">
           Details
         </Link>
       </div>
