b7e82f1809cbb1b8f7652540ac62cca23373d9a0
rikimario <mario_martinov@abv.bg>
2023-11-20 16:46:19 +0200

    add getAll func to services

--- Routing-Workshop/client/src/services/gameService.js
+++ Routing-Workshop/client/src/services/gameService.js
@@ -1,7 +1,15 @@
-const baseURL = "http://localhost:3030/jsonstore";
+import { request } from "../lib/request";
+
+const baseURL = "http://localhost:3030/jsonstore/games";
+
+export const getAll = async () => {
+  const result = request("GET", baseURL);
+
+  return Object.values(result);
+};
 
 export const create = async (gameData) => {
-  const response = await fetch(`${baseURL}/games`, {
+  const response = await fetch(baseURL, {
     method: "POST",
     headers: {
       "content-type": "aplication/json",
