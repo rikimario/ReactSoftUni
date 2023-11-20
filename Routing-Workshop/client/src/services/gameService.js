3e11a93dd37a47d6f5c680725ad215d80b6e9873
rikimario <mario_martinov@abv.bg>
2023-11-20 21:04:03 +0200

    add requests

--- Routing-Workshop/client/src/services/gameService.js
+++ Routing-Workshop/client/src/services/gameService.js
@@ -1,22 +1,15 @@
-import { request } from "../lib/request";
+import * as request from "../lib/request";
 
 const baseURL = "http://localhost:3030/jsonstore/games";
 
 export const getAll = async () => {
-  const result = await request("GET", baseURL);
+  const result = await request.get(baseURL);
 
   return Object.values(result);
 };
 
 export const create = async (gameData) => {
-  const response = await fetch(baseURL, {
-    method: "POST",
-    headers: {
-      "content-type": "application/json",
-    },
-    body: JSON.stringify(gameData),
-  });
-  const result = await response.json();
+  const result = await request.post(baseURL, gameData);
 
   return result;
 };
