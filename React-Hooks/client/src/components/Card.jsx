cd68876c0a67927ea40860cfc488e437fd5e48e4
rikimario <mario_martinov@abv.bg>
2023-11-21 16:04:02 +0200

    add title is finish and -id to cards

--- React-Hooks/client/src/components/Card.jsx
+++ React-Hooks/client/src/components/Card.jsx
@@ -1,12 +1,12 @@
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 
-export default function Cards() {
+export default function Cards({ _id, title, isFinish }) {
   return (
     <Card style={{ width: "18rem" }}>
       <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>state: </Card.Text>
+        <Card.Title>{title}</Card.Title>
+        <Card.Text>state: {isFinish}</Card.Text>
         <Button variant="primary">Change</Button>
       </Card.Body>
     </Card>
