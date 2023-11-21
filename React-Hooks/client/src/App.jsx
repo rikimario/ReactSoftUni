3a33b251369b0f41b03742e7dd5f43a920382eb9
rikimario <mario_martinov@abv.bg>
2023-11-21 20:38:44 +0200

    create on submit handler and add it ti card container

--- React-Hooks/client/src/App.jsx
+++ React-Hooks/client/src/App.jsx
@@ -3,13 +3,13 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Nav from "./components/Navigatio";
 import CardContainer from "./components/CardContainer";
 
-const baseUrl = "http://localhost:3030/jsonstore/";
+const baseUrl = "http://localhost:3030/jsonstore/todos";
 
 function App() {
   const [todos, setTodos] = useState([]);
 
   useEffect(() => {
-    fetch(`${baseUrl}todos`)
+    fetch(baseUrl)
       .then((response) => {
         return response.json();
       })
@@ -17,10 +17,22 @@ function App() {
         setTodos(Object.values(result));
       });
   }, []);
+
+  const onSubmitHandler = async (value) => {
+    const response = await fetch(baseUrl, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ ...value, isFinish: "false" }),
+    });
+
+    const data = await response.json();
+    setTodos((state) => [...state, data]);
+  };
+
   return (
     <>
       <Nav />
-      <CardContainer todos={todos} />
+      <CardContainer todos={todos} onSubmitHandler={onSubmitHandler} />
     </>
   );
 }
