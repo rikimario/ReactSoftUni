435bbd0c56df609228786b2aed6315b6a6a81904
rikimario <mario_martinov@abv.bg>
2023-11-22 00:04:52 +0200

    add useForm to login

--- Routing-Workshop/client/src/components/Login/Login.jsx
+++ Routing-Workshop/client/src/components/Login/Login.jsx
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom";
+import useForm from "../../hooks/useForm";
 
 export default function Login() {
+  const { values, onChange, onSubmit } = useForm({
+    email: "",
+    password: "",
+  });
+
   return (
     <section id="login-page" className="auth">
-      <form id="login">
+      <form id="login" onSubmit={onSubmit}>
         <div className="container">
           <div className="brand-logo" />
           <h1>Login</h1>
@@ -13,9 +19,17 @@ export default function Login() {
             id="email"
             name="email"
             placeholder="Sokka@gmail.com"
+            onChange={onChange}
+            value={values.email}
           />
           <label htmlFor="login-pass">Password:</label>
-          <input type="password" id="login-password" name="password" />
+          <input
+            type="password"
+            id="login-password"
+            name="password"
+            onChange={onChange}
+            value={values.password}
+          />
           <input type="submit" className="btn submit" defaultValue="Login" />
           <p className="field">
             <span>
