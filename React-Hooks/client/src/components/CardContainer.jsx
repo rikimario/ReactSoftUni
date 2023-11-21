e566550a6964fe6cb465517bc26a4f0bf8bf39e4
rikimario <mario_martinov@abv.bg>
2023-11-21 20:40:25 +0200

    add addTodo to card container and on submit handler

--- React-Hooks/client/src/components/CardContainer.jsx
+++ React-Hooks/client/src/components/CardContainer.jsx
@@ -1,11 +1,18 @@
+import AddTodo from "./AddTodo";
 import Cards from "./Card";
 
-export default function CardContainer({ todos }) {
+export default function CardContainer({ todos, onSubmitHandler }) {
   return (
-    <div style={{ margin: "20px" }}>
-      {todos.map((item) => (
-        <Cards key={item._id} {...item} />
-      ))}
-    </div>
+    <>
+      <div style={{ margin: "20px", display: "flex", flexDirection: "row" }}>
+        {todos.map((item) => (
+          <Cards key={item._id} {...item} />
+        ))}
+      </div>
+
+      <div>
+        <AddTodo onSubmitHandler={onSubmitHandler} />
+      </div>
+    </>
   );
 }
