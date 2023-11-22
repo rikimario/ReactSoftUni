157fee2e887b2e897acc14c6b3a230af7d6e0a9d
rikimario <mario_martinov@abv.bg>
2023-11-22 14:36:07 +0200

    remove prop context and add useContext

--- Routing-Workshop/client/src/components/Login/Login.jsx
+++ Routing-Workshop/client/src/components/Login/Login.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
 import useForm from "../../hooks/useForm";
+import { useContext } from "react";
+import AuthContext from "../../contexts/authContext";
 
 const LoginFormKeys = {
   Email: "email",
   Password: "password",
 };
 
-export default function Login({ loginSubmitHandler }) {
+export default function Login() {
+  const { loginSubmitHandler } = useContext(AuthContext);
+
   const { values, onChange, onSubmit } = useForm(loginSubmitHandler, {
     [LoginFormKeys.Email]: "",
     [LoginFormKeys.Password]: "",
