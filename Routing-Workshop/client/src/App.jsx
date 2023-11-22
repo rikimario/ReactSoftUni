ad003aefdb107e7dc4d60e45bc9114f10e700e80
rikimario <mario_martinov@abv.bg>
2023-11-22 14:36:28 +0200

    add auth context provider

--- Routing-Workshop/client/src/App.jsx
+++ Routing-Workshop/client/src/App.jsx
@@ -7,6 +7,7 @@ import Login from "./components/Login/Login";
 import Register from "./components/Register/Register";
 import Details from "./components/Details/Details";
 import { useState } from "react";
+import AuthContext from "./contexts/authContext";
 
 function App() {
   const [auth, setAuth] = useState({});
@@ -16,21 +17,20 @@ function App() {
   };
 
   return (
-    <div id="box">
-      <Header />
+    <AuthContext.Provider value={{ loginSubmitHandler }}>
+      <div id="box">
+        <Header />
 
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/games" element={<GameList />} />
-        <Route path="/create-game" element={<Create />} />
-        <Route
-          path="/login"
-          element={<Login loginSubmitHandler={loginSubmitHandler} />}
-        />
-        <Route path="/register" element={<Register />} />
-        <Route path="/games/:gameId" element={<Details />} />
-      </Routes>
-    </div>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/games" element={<GameList />} />
+          <Route path="/create-game" element={<Create />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/games/:gameId" element={<Details />} />
+        </Routes>
+      </div>
+    </AuthContext.Provider>
   );
 }
 
