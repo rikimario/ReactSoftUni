98bd592fb6950305656135aab11871ecf25dfc21
rikimario <mario_martinov@abv.bg>
2023-11-22 23:33:08 +0200

    change jsonstore to data and remove object form values

--- Routing-Workshop/client/src/services/gameService.js
+++ Routing-Workshop/client/src/services/gameService.js
@@ -1,11 +1,11 @@
 import * as request from "../lib/request";
 
-const baseURL = "http://localhost:3030/jsonstore/games";
+const baseURL = "http://localhost:3030/data/games";
 
 export const getAll = async () => {
   const result = await request.get(baseURL);
 
-  return Object.values(result);
+  return result;
 };
 
 export const getOne = async (gameId) => {
